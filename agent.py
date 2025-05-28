6d220239720dd8954b805ecc5ebd10e84394485d
krishnnavyas36 <krishnavyastr@gmail.com>
2025-05-27 20:37:17 -0700

    comment added

--- agent.py
+++ agent.py
@@ -4,7 +4,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
-
+#krishnavyas
 def load_data(filepath):
     try:
         df = pd.read_csv(filepath)
